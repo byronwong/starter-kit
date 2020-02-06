e2185d447885eb15e4e40462a87c7839139027b7
byronwong <5236867+byronwong@users.noreply.github.com>
2020-02-06 17:11:36 +0000

    cleaning up files

--- app.js
+++ app.js
@@ -4,26 +4,13 @@ import favicon from 'serve-favicon';
 import Chalk from 'chalk';
 import opn from 'opn';
 
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-
 const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(favicon(path.resolve(__dirname, 'favicon.ico')));
 
-// links express with webpack middleware
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
-
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}));
-
 // File server
-app.use(express.static('dist'));
+app.use(express.static('public'));
 
 // Routes
 app.get('/', function(req, res){
