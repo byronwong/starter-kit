7f797005e084cd165282835ab24c2a065ddf4e29
byronwong <5236867+byronwong@users.noreply.github.com>
2020-02-06 17:34:49 +0000

    refactored code with pure express

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-import path from 'path';
-import express from 'express';
-import favicon from 'serve-favicon';
-import Chalk from 'chalk';
-import opn from 'opn';
+const path = require('path')
+const express = require('express')
+const serveFavicon = require('serve-favicon')
+const Chalk = require('chalk')
+const opn = require('opn')
+
+// import express from 'express';
+// import favicon from 'serve-favicon';
+// import Chalk from 'chalk';
+// import opn from 'opn';
 
 const port = process.env.PORT || 8000;
 const app = express();
 
-app.use(favicon(path.resolve(__dirname, 'favicon.ico')));
+app.use(serveFavicon(path.resolve(__dirname, 'favicon.ico')));
 
 // File server
 app.use(express.static('public'));
 
 // Routes
 app.get('/', function(req, res){
-  res.sendFile('index.html', {root: path.resolve(__dirname, 'dist')});
+  res.sendFile('index.html', {root: path.resolve(__dirname, 'public')});
 });
 
 app.listen(port, function(err){
