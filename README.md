6d79e8be565699304cf60b4b84b6e70beb68849c
byronwong <byronwong16@gmail.com>
2021-12-31 18:29:19 +0000

    Added Asset Management

--- README.md
+++ README.md
@@ -6,8 +6,12 @@ This is a basic starter-kit, ideally for sandbox and very early prototypes.
 
 ## Webpack - Basic Setup
 
+- Webpack Version 5
 - Build automation with web pack
 - https://webpack.js.org/guides/getting-started/#basic-setup
+- Added CSS bundling via `style-loader` and `css-loader`
+- Checkout JSON loading for text content loading - https://webpack.js.org/guides/asset-management/#customize-parser-of-json-modules
+- Use `./src/assets` for global assets
 
 # TODO
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import _ from "lodash";
+import _ from 'lodash';
+
+import './style.css';
+
+// import TestPic from './assets/images/pawel-czerwinski-oykbZuvajO0-unsplash.jpg';
 
 // => Note to future self
 // - This tests Webpack is working
-// - You can choose not to use this and directly update the index.html
+// - You can choose not to use this and directly update the index.html in dist
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
